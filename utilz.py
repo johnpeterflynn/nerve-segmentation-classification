fa8d5e427e109c12e89ebcc8926c1f5a5b18ed76
Florian Hinterwimmer <florian.hinterwimmer@tum.de>
2019-08-28 11:14:34 +0200

    added elastic deformation

--- utilz.py
+++ utilz.py
@@ -4,6 +4,8 @@ import torch
 from scipy import misc
 import matplotlib.pyplot as plt
 import os
+from scipy.ndimage.interpolation import map_coordinates
+from scipy import interpolate as ipol
 
 np.seterr(divide='ignore', invalid='ignore')
 
@@ -34,8 +36,7 @@ def load_files(filename):
 
 
 def show_labels(image, labels, prediction, results_path, i):
-    
-    _, a, b = np.where(labels != 0)
+    _, a, b, _ = np.where(labels != 0)
     e, f = np.where(prediction != 0)
     _, x, y, z = image.shape
     image = image[0,-1,:,:] ### pick one spectrum just to show image+labels
@@ -43,35 +44,16 @@ def show_labels(image, labels, prediction, results_path, i):
     
     plt.figure()
     plt.imshow(image, cmap='gray')
-    aa = plt.scatter(b, a, s=1, marker='o', c='red', alpha=0.5)
+    aa = plt.scatter(b, a, s=1, marker='o', c='red', alpha=0.6)
     aa.set_label('label')
     plt.legend()
-    bb = plt.scatter(f, e, s=2, marker='o', c='blue', alpha=0.1)
+    bb = plt.scatter(f, e, s=2, marker='o', c='blue', aplha=0.3)
     bb.set_label('prediction')
     plt.legend()
     plt.axis('off')
     plt.savefig(os.path.join(results_path, 'label plus pred' + str(i) + '.png'))
     
     
-    
-def median_frequency_balancing(labels):
-    
-    Labels = np.unique(labels)
-    NumClass = len(Labels)
-    ClassWeights = np.zeros((NumClass))
-    ClassFreq = np.zeros((1, NumClass))
-    
-    # Estimate Class Frequency
-    for i in range(NumClass):
-        #l = labels
-        pixel_in_this_class, test = np.where(labels.astype(int) == i)
-        ClassFreq[0, i] = len(pixel_in_this_class) / len(labels)
-    
-    MedianFreq = np.median(ClassFreq)
-    
-    for j in range(NumClass):
-        ClassWeights[j] = MedianFreq / ClassFreq[0,j]        
-    return ClassWeights
 
 
 
@@ -89,6 +71,91 @@ def norm(ar):
     ar /= np.ptp(ar, axis=0)
     return ar
 
+
+
+
+### from Quicknat, preprocess.py
+def estimate_weights_mfb(labels):
+    class_weights = np.zeros_like(labels)
+    unique, counts = np.unique(labels, return_counts=True)
+    median_freq = np.median(counts)
+    weights = np.zeros(len(unique))
+    for i, label in enumerate(unique):
+        class_weights += (median_freq // counts[i]) * np.array(labels == label)
+        weights[int(label)] = median_freq // counts[i]
+    grads = np.gradient(labels)
+    edge_weights = (grads[0] ** 2 + grads[1] ** 2) > 0
+    class_weights += 2 * edge_weights
+    return class_weights, weights
+
+
+
+
+# deform function
+def elastic_deformation(image, x_coord, y_coord, dx, dy):
+    """ Applies random elastic deformation to the input image 
+        with given coordinates and displacement values of deformation points.
+        Keeps the edge of the image steady by adding a few frame points that get displacement value zero.
+    Input: image: array of shape (N.M,C) (Haven't tried it out for N != M), C number of channels
+           x_coord: array of shape (L,) contains the x coordinates for the deformation points
+           y_coord: array of shape (L,) contains the y coordinates for the deformation points
+           dx: array of shape (L,) contains the displacement values in x direction
+           dy: array of shape (L,) contains the displacement values in x direction
+    Output: the deformed image (shape (N,M,C))
+    """
+        
+    ## Preliminaries    
+    # dimensions of the input image
+    shape = image.shape
+        
+    # centers of x and y axis
+    x_center = shape[1]/2
+    y_center = shape[0]/2
+    
+    ## Construction of the coarse grid
+    # deformation points: coordinates
+            
+    # anker points: coordinates    
+    x_coord_anker_points = np.array([0, x_center, shape[1] - 1, 0, shape[1] - 1, 0, x_center, shape[1] - 1])
+    y_coord_anker_points = np.array([0, 0, 0, y_center, y_center, shape[0] - 1, shape[0] - 1, shape[0] - 1])
+    # anker points: values
+    dx_anker_points = np.zeros(8)
+    dy_anker_points = np.zeros(8)
+    
+    # combine deformation and anker points to coarse grid 
+    x_coord_coarse = np.append(x_coord, x_coord_anker_points)
+    y_coord_coarse = np.append(y_coord, y_coord_anker_points)
+    coord_coarse = np.array(list(zip(x_coord_coarse, y_coord_coarse)))
+    
+    dx_coarse = np.append(dx, dx_anker_points)
+    dy_coarse = np.append(dy, dy_anker_points)
+        
+    ## Interpolation onto fine grid
+    # coordinates of fine grid
+    coord_fine = [[x,y] for x in range(shape[1]) for y in range(shape[0])]
+    # interpolate displacement in both x and y direction
+    dx_fine = ipol.griddata(coord_coarse, dx_coarse, coord_fine, method = 'cubic') # cubic works better but takes longer
+    dy_fine = ipol.griddata(coord_coarse, dy_coarse, coord_fine, method = 'cubic') # other options: 'linear'
+    # get the displacements into shape of the input image (the same values in each channel)
+    
+
+
+    
+    dx_fine = dx_fine.reshape(shape[0:2])
+    dx_fine = np.stack([dx_fine]*shape[2], axis = -1)
+    dy_fine = dy_fine.reshape(shape[0:2])
+    dy_fine = np.stack([dy_fine]*shape[2], axis = -1)
+    
+    ## Deforming the image: apply the displacement grid
+    # base grid 
+    x, y, z = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), np.arange(shape[2]))
+    # add displacement to base grid (-> new coordinates)
+    indices = np.reshape(y+dy_fine, (-1, 1)), np.reshape(x+dx_fine, (-1, 1)), np.reshape(z, (-1, 1))
+    # evaluate the image at the new coordinates
+    deformed_image = map_coordinates(image, indices, order=2, mode='nearest')
+    deformed_image = deformed_image.reshape(image.shape)
+    
+    return deformed_image
 
 
 
