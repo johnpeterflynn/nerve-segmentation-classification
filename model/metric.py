a14b07319db9126280a39a5c4897ae940a0a4e7b
Farid Yagubbayli <faridyagubbayli@gmail.com>
2019-12-06 23:36:23 +0100

    Optimization of metrics calculations

--- model/metric.py
+++ model/metric.py
@@ -43,31 +43,28 @@ def ged(samples, targets):
     m = targets.shape[1]
     n = samples.shape[1]
 
-    geds = torch.zeros(batch_size).float()
-    # TODO tensorize
-    for b in range(batch_size):
-        # Average GED between samples and ground truths
-        first_term = torch.tensor(0, dtype=torch.float)
-        for sample_i in range(n):
-            for gt_j in range(m):
-                first_term += _ged_dist_func(samples[b, sample_i, ...], targets[b, gt_j, ...])
-        first_term = (2 / (n * m)) * first_term.float()
+    # Average GED between samples and ground truths
+    first_term = torch.zeros(batch_size, dtype=torch.float)
+    for sample_i in range(n):
+        for gt_j in range(m):
+            first_term += _ged_dist_func(samples[:, sample_i, ...], targets[:, gt_j, ...])
+    first_term = (2 / (n * m)) * first_term.float()
 
-        # Average GED between samples
-        second_term = torch.tensor(0, dtype=torch.float)
-        for sample_i in range(n):
-            for sample_j in range(n):
-                second_term = _ged_dist_func(samples[b, sample_i, ...], samples[b, sample_j, ...])
-        second_term = (1 / (n ** 2)) * second_term.float()
+    # Average GED between samples
+    second_term = torch.zeros(batch_size, dtype=torch.float)
+    for sample_i in range(n):
+        for sample_j in range(n):
+            second_term = _ged_dist_func(samples[:, sample_i, ...], samples[:, sample_j, ...])
+    second_term = (1 / (n ** 2)) * second_term.float()
 
-        # Average GED between ground truths
-        third_term = torch.tensor(0, dtype=torch.float)
-        for gt_i in range(m):
-            for gt_j in range(m):
-                third_term += _ged_dist_func(targets[b, gt_i, ...], targets[b, gt_j, ...])
-        third_term = (1 / (m ** 2)) * third_term.float()
+    # Average GED between ground truths
+    third_term = torch.zeros(batch_size, dtype=torch.float)
+    for gt_i in range(m):
+        for gt_j in range(m):
+            third_term += _ged_dist_func(targets[:, gt_i, ...], targets[:, gt_j, ...])
+    third_term = (1 / (m ** 2)) * third_term.float()
 
-        geds[b] = first_term - second_term - third_term
+    geds = first_term - second_term - third_term
 
     return geds.mean().cpu().item()
 
@@ -85,37 +82,25 @@ def dice_agreement_in_samples(samples, _=None):
     num_labels = samples.shape[2]
     dice_per_label = torch.zeros((batch_size, num_labels), dtype=torch.float)
 
-    for b in range(batch_size):
-        num_pairs = 0
-        for i, j in combinations(list(range(n)), 2):
-            inp1 = samples[b, i, ...]
-            inp2 = samples[b, j, ...]
+    num_pairs = 0
+    for i, j in combinations(list(range(n)), 2):
+        inp1 = samples[:, i, ...]
+        inp2 = samples[:, j, ...]
 
-            # Compute number of pixels intersecting
-            intersection = inp1 * inp2
-            sum_intersection = intersection.view(num_labels, -1).sum(1)
-            # Compute number of pixels in union
-            union = (inp1 + inp2)
-            sum_union = (union > 0).view(num_labels, -1).sum(1)  # Will exclude double-intersection as well
+        # Compute number of pixels intersecting
+        intersection = inp1 * inp2
+        sum_intersection = intersection.view(num_labels, -1).sum(1)
+        # Compute number of pixels in union
+        union = (inp1 + inp2)
+        sum_union = (union > 0).view(num_labels, -1).sum(1)  # Will exclude double-intersection as well
 
-            for label_i in range(num_labels):
-                sum_i = sum_intersection[label_i].float()
-                sum_u = sum_union[label_i].float()
+        sum_i = sum_intersection[:].float()
+        sum_u = sum_union[:].float()
+        dice_per_label[:, :] += (sum_i.float() + 1e-6) / (sum_u + 1e-6)
 
-                if sum_i == 0 and sum_u == 0:
-                    dice_per_label[b, label_i] += torch.tensor(1, dtype=torch.float)
+        num_pairs += 1
 
-                elif (sum_i > 0 and sum_u == 0) or \
-                        (sum_i == 0 and sum_u > 0):
-                    dice_per_label[b, label_i] += torch.tensor(0, dtype=torch.float)
-
-                else:
-                    dice_per_label[b, label_i] += sum_i.float() / sum_u
-
-            num_pairs += 1
-
-        dice_per_label[b, :] /= num_pairs
-
+    dice_per_label[:, :] /= num_pairs
     return dice_per_label.mean().cpu().item()
 
 
@@ -134,20 +119,20 @@ def iou_samples_per_label(samples, _=None):
 
     dice_per_label = torch.zeros((batch_size, num_labels)).float().to(_device)
 
-    for b in range(batch_size):
-        # Size of intersection and union is [NUM_CHANNELS x H x W]
-        intersection = torch.ones(samples.shape[2:]).long().to(_device)
-        union = torch.zeros(samples.shape[2:]).long().to(_device)
+    # Size of intersection and union is [NUM_CHANNELS x H x W]
+    img_size = (batch_size, *samples.shape[2:])
+    intersection = torch.ones(img_size).long().to(_device)
+    union = torch.zeros(img_size).long().to(_device)
 
-        # Intersection and union over all samples
-        for i in range(n):
-            intersection *= samples[b, i, ...].long()
-            union += samples[b, i, ...].long()
+    # Intersection and union over all samples
+    for i in range(n):
+        intersection *= samples[:, i, ...].long()
+        union += samples[:, i, ...].long()
 
-        sum_intersection = intersection.view(num_labels, -1).sum(1)
-        sum_union = (union > 0).view(num_labels, -1).sum(1)  # Will exclude double-intersection as well
+    sum_intersection = intersection.view(num_labels, -1).sum(1)
+    sum_union = (union > 0).view(num_labels, -1).sum(1)  # Will exclude double-intersection as well
 
-        dice_per_label[b, ...] = (sum_intersection.float() + 1e-6) / (sum_union.float() + 1e-6)
+    dice_per_label[:, ...] = (sum_intersection.float() + 1e-6) / (sum_union.float() + 1e-6)
 
     return dice_per_label.mean().cpu().item()
 
@@ -163,14 +148,14 @@ def pixel_wise_ce_samples(samples):
     :return: Tensor of shape [H x W]
     """
     samples = samples.float()
-    N = samples.shape[0]
+    batch_size, N = samples.shape[0], samples.shape[1]
 
-    mean_samples = samples.mean(0)
+    mean_samples = samples.mean(1)
 
-    gamma_maps = torch.zeros((N, samples.shape[2], samples.shape[3]), dtype=torch.float).to(_device)
+    gamma_maps = torch.zeros((batch_size, N, samples.shape[3], samples.shape[4]), dtype=torch.float).to(_device)
     for i in range(N):
-        gamma_maps[i, ...] += _pixel_wise_xent(samples[i, ...], mean_samples)
-    gamma_map = gamma_maps.mean(0)
+        gamma_maps[:, i, ...] += _pixel_wise_xent(samples[:, i, ...], mean_samples)
+    gamma_map = gamma_maps.mean(1)
 
     return gamma_map
 
@@ -193,24 +178,23 @@ def variance_ncc_samples(samples, g_truths):
     M = g_truths.shape[1]
 
     batch_nccs = torch.zeros(batch_size).float().to(_device)
-    for b in range(batch_size):
-        batch_samples = samples[b, ...].float()
-        batch_g_truths = g_truths[b, ...].float()
+    batch_samples = samples[:, ...].float()
+    batch_g_truths = g_truths[:, ...].float()
 
-        gamma_map_ss = pixel_wise_ce_samples(batch_samples)
+    gamma_map_ss = pixel_wise_ce_samples(batch_samples)
 
-        E_sy_arr = torch.zeros((M, N, batch_samples.size()[2], batch_samples.size()[3]))
-        for j in range(M):
-            for i in range(N):
-                E_sy_arr[j, i, ...] = _pixel_wise_xent(batch_samples[i, ...], batch_g_truths[j, ...])
+    E_sy_arr = torch.zeros((batch_size, M, N, batch_samples.size()[3], batch_samples.size()[4]))
+    for j in range(M):
+        for i in range(N):
+            E_sy_arr[:, j, i, ...] = _pixel_wise_xent(batch_samples[:, i, ...], batch_g_truths[:, j, ...])
 
-        E_sy = E_sy_arr.mean(dim=1)
+    E_sy = E_sy_arr.mean(dim=2)
 
-        nccs = torch.zeros(M).float()
-        for j in range(M):
-            nccs[j] = _ncc(gamma_map_ss, E_sy[j, ...])
+    nccs = torch.zeros((batch_size, M)).float()
+    for j in range(M):
+        nccs[:, j] = _ncc(gamma_map_ss, E_sy[:, j, ...])
 
-        batch_nccs[b] = nccs.mean()
+    batch_nccs[:] = nccs.mean(dim=1)
 
     return torch.mean(batch_nccs).cpu().item()
 
@@ -219,32 +203,26 @@ def _ged_dist_func(inp1: torch.Tensor, inp2: torch.Tensor):
     inp1 = inp1.float()
     inp2 = inp2.float()
 
-    inp_size = inp1.size()
-    num_labels = inp_size[0]
+    batch_size, num_labels = inp1.shape[0], inp1.shape[1]
 
     intersection = inp1 * inp2
-    sum_intersection = intersection.view(num_labels, -1).sum(1)
+    sum_intersection = intersection.view(batch_size, num_labels, -1).sum(2)
     union = (inp1 + inp2)
-    sum_union = (union > 0).view(num_labels, -1).sum(1)  # Will exclude double-intersection as well
+    sum_union = (union > 0).view(batch_size, num_labels, -1).sum(2)  # Will exclude double-intersection as well
 
-    per_label_iou = torch.zeros(num_labels).float()
-    for label_i in range(inp_size[0]):
-        sum_i = sum_intersection[label_i]
-        sum_u = sum_union[label_i]
+    per_label_iou = torch.zeros(batch_size, num_labels).float()
 
-        if sum_i == 0 and sum_u == 0:
-            per_label_iou[label_i] = torch.tensor(1, dtype=torch.float)
-        elif (sum_i > 0 and sum_u == 0) or (sum_i == 0 and sum_u > 0):
-            per_label_iou[label_i] = torch.tensor(0, dtype=torch.float)
-        else:
-            per_label_iou[label_i] = sum_i.float() / sum_u
+    sum_i = sum_intersection[:]
+    sum_u = sum_union[:]
+    per_label_iou[:] = (sum_i.float() + 1e-6) / (sum_u + 1e-6)
 
-    return 1 - per_label_iou.mean()
+
+    return 1 - per_label_iou.mean(dim=1)
 
 
 def _pixel_wise_xent(sample, gt, eps=1e-8):
     log_sample = torch.log(sample + eps)
-    return -1 * torch.sum(gt * log_sample, 0)
+    return -1 * torch.sum(gt * log_sample, 1)
 
 
 def _ncc(a,v, zero_norm=True, eps=1e-8):
