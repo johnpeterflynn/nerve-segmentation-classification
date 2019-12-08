1fb3022e29160e1aa9201ce14398c13907deb0f5
Mohammad Alamleh <malamleh93@gmail.com>
2019-12-08 21:00:19 +0100

    Update opus tester
    Signed-off-by: Mohammad Alamleh <malamleh93@gmail.com>

--- runners/opus_tester.py
+++ runners/opus_tester.py
@@ -1,24 +1,29 @@
-from parse_config import ConfigParser
-from base import BaseRunner, CustomArgs
-import model as module_arch
-import model.metric as module_metric
-import model.loss as module_loss
-import data_loaders as module_data
-from tqdm import tqdm
-import torch
 import argparse
 import os
 import sys
 
+
 # This is important to be able to call other modules
 # in the upper directory (root dir for our code)
 sys.path.append(os.getcwd())
+
+import torch
+from tqdm import tqdm
+
+import data_loaders as module_data
+import model as module_arch
+import model.loss as module_loss
+import model.metric as module_metric
+from base import BaseRunner, CustomArgs
+from parse_config import ConfigParser
 
 
 class OpusTester(BaseRunner):
 
     def __init__(self):
         super().__init__("OpusTester")
+        self.metrics_sample_count = 1
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     def add_static_arguments(self):
         super().add_static_arguments()
@@ -26,9 +31,25 @@ class OpusTester(BaseRunner):
     def add_dynamic_arguments(self):
         super().add_dynamic_arguments()
 
+    def _sample(self, model, data):
+        num_samples = self.metrics_sample_count
+
+        batch_size, num_channels, image_size = data.shape[0], 1, tuple(
+            data.shape[2:])
+        samples = torch.zeros(
+            (batch_size, num_samples, num_channels, *image_size)).to(self.device)
+        for i in range(num_samples):
+            output = model(data)
+
+            _, idx = torch.max(output, 1)
+            sample = idx.unsqueeze(dim=1)
+            samples[:, i, ...] = sample
+
+        return samples
+
     def _run(self, config):
         logger = config.get_logger('test')
-
+        self.metrics_sample_count = config['trainer']['metrics_sample_count']
         # setup data_loader instances
         data_loader = getattr(module_data, config['data_loader']['type'])(
             config['data_loader']['args']['data_dir'],
@@ -48,10 +69,9 @@ class OpusTester(BaseRunner):
         metric_fns = [getattr(module_metric, met) for met in config['metrics']]
 
         logger.info('Loading checkpoint: {} ...'.format(config.resume))
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         checkpoint = torch.load(
-            config.resume, map_location=torch.device(device))
+            config.resume, map_location=torch.device(self.device))
 
         state_dict = checkpoint['state_dict']
         if config['n_gpu'] > 1:
@@ -59,27 +79,30 @@ class OpusTester(BaseRunner):
         model.load_state_dict(state_dict)
 
         # prepare model for testing
-        model = model.to(device)
+        model = model.to(self.device)
         model.eval()
 
         total_loss = 0.0
         total_metrics = torch.zeros(len(metric_fns))
 
         with torch.no_grad():
-            for i, (data, target) in enumerate(tqdm(data_loader)):
-                data, target = data.to(device), target.to(device)
+            for _, (data, target) in enumerate(tqdm(data_loader)):
+                data, target = data.to(self.device), target.to(self.device)
                 output = model(data)
 
-                #
-                # save sample images, or do something with output here
-                #
+                # [BATCH_SIZE x SAMPLE_SIZE x NUM_CHANNELS x H x W]
+                samples = self._sample(model, data)
 
                 # computing loss, metrics on test set
                 loss = loss_fn(output, target)
                 batch_size = data.shape[0]
                 total_loss += loss.item() * batch_size
                 for i, metric in enumerate(metric_fns):
-                    total_metrics[i] += metric(output, target) * batch_size
+                    if metric.__name__ in ["ged", "dice_agreement_in_samples", "iou_samples_per_label", "variance_ncc_samples"]:
+                        total_metrics[i] += metric(samples,
+                                                   target) * batch_size
+                    else:
+                        total_metrics[i] += metric(output, target) * batch_size
 
         n_samples = len(data_loader.sampler)
         log = {'loss': total_loss / n_samples}
