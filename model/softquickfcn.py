672b4b4b4863d367d4e2c278bd3e951467a44e1f
John Flynn <johnflynn90@gmail.com>
2020-01-31 11:14:51 +0100

    Add reduced quicknat model to softquicknat

--- model/softquickfcn.py
+++ model/softquickfcn.py
@@ -6,6 +6,223 @@ from nn_common_modules import modules as sm
 from squeeze_and_excitation import squeeze_and_excitation as se
 from base import BaseModel
 
+class GroupNorm(nn.Module):
+    def __init__(self, num_features, num_groups=32, eps=1e-5):
+        super(GroupNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(1,num_features,1,1))
+        self.bias = nn.Parameter(torch.zeros(1,num_features,1,1))
+        self.num_groups = num_groups
+        self.eps = eps
+
+    def forward(self, x):
+        N,C,H,W = x.size()
+        G = self.num_groups
+        assert C % G == 0
+
+        x = x.view(N,G,-1)
+        mean = x.mean(-1, keepdim=True)
+        var = x.var(-1, keepdim=True)
+
+        x = (x-mean) / (var+self.eps).sqrt()
+        x = x.view(N,C,H,W)
+        return x * self.weight + self.bias
+
+
+class DenseBlock(nn.Module):
+    """Block with dense connections
+
+    :param params: {
+        'num_channels':1,
+        'num_filters':64,
+        'kernel_h':5,
+        'kernel_w':5,
+        'stride_conv':1,
+        'pool':2,
+        'stride_pool':2,
+        'num_classes':28,
+        'se_block': se.SELayer.None,
+        'drop_out':0,2}
+    :type params: dict
+    :param se_block_type: Squeeze and Excite block type to be included, defaults to None
+    :type se_block_type: str, valid options are {'NONE', 'CSE', 'SSE', 'CSSE'}, optional
+    :return: forward passed tensor
+    :rtype: torch.tonsor [FloatTensor]
+    """
+
+    def __init__(self, params, se_block_type=None):
+        super(DenseBlock, self).__init__()
+
+        if se_block_type == se.SELayer.CSE.value:
+            self.SELayer = se.ChannelSELayer(params['num_filters'])
+
+        elif se_block_type == se.SELayer.SSE.value:
+            self.SELayer = se.SpatialSELayer(params['num_filters'])
+
+        elif se_block_type == se.SELayer.CSSE.value:
+            self.SELayer = se.ChannelSpatialSELayer(params['num_filters'])
+        else:
+            self.SELayer = None
+
+        padding_h = int((params['kernel_h'] - 1) / 2)
+        padding_w = int((params['kernel_w'] - 1) / 2)
+
+        conv1_out_size = int(params['num_channels'] + params['num_filters'])
+        conv2_out_size = int(
+            params['num_channels'] + params['num_filters'] + params['num_filters'])
+
+        self.conv1 = nn.Conv2d(in_channels=params['num_channels'], out_channels=params['num_filters'],
+                               kernel_size=(
+                                   params['kernel_h'], params['kernel_w']),
+                               padding=(padding_h, padding_w),
+                               stride=params['stride_conv'])
+        self.conv2 = nn.Conv2d(in_channels=conv1_out_size, out_channels=params['num_filters'],
+                               kernel_size=(
+                                   params['kernel_h'], params['kernel_w']),
+                               padding=(padding_h, padding_w),
+                               stride=params['stride_conv'])
+        self.conv3 = nn.Conv2d(in_channels=conv2_out_size, out_channels=params['num_filters'],
+                               kernel_size=(1, 1),
+                               padding=(0, 0),
+                               stride=params['stride_conv'])
+        # self.norm1 = nn.BatchNorm2d(num_features=params['num_channels'], momentum=0.9)
+        # self.norm2 = nn.BatchNorm2d(num_features=conv1_out_size, momentum=0.9)
+        # self.norm3 = nn.BatchNorm2d(num_features=conv2_out_size, momentum=0.9)
+        # self.norm1 = GroupNorm(num_features=params['num_channels'], num_groups=2)
+        # self.norm2 = GroupNorm(num_features=conv1_out_size, num_groups=2)
+        # self.norm3 = GroupNorm(num_features=conv2_out_size, num_groups=2)
+        self.norm1 = nn.InstanceNorm2d(num_features=params['num_channels'])
+        self.norm2 = nn.InstanceNorm2d(num_features=conv1_out_size)
+        self.norm3 = nn.InstanceNorm2d(num_features=conv2_out_size)
+        self.prelu = nn.PReLU()
+        if params['drop_out'] > 0:
+            self.drop_out_needed = True
+            self.drop_out = nn.Dropout2d(params['drop_out'])
+        else:
+            self.drop_out_needed = False
+
+    def forward(self, input):
+        """Forward pass
+
+        :param input: Input tensor, shape = (N x C x H x W)
+        :type input: torch.tensor [FloatTensor]
+        :return: Forward passed tensor
+        :rtype: torch.tensor [FloatTensor]
+        """
+
+        o1 = self.norm1(input)
+        o2 = self.prelu(o1)
+        o3 = self.conv1(o2)
+        o4 = torch.cat((input, o3), dim=1)
+        o5 = self.norm2(o4)
+        o6 = self.prelu(o5)
+        o7 = self.conv2(o6)
+        o8 = torch.cat((input, o3, o7), dim=1)
+        o9 = self.norm3(o8)
+        o10 = self.prelu(o9)
+        out = self.conv3(o10)
+        return out
+
+
+class EncoderBlock(DenseBlock):
+    """Dense encoder block with maxpool and an optional SE block
+
+    :param params: {
+        'num_channels':1,
+        'num_filters':64,
+        'kernel_h':5,
+        'kernel_w':5,
+        'stride_conv':1,
+        'pool':2,
+        'stride_pool':2,
+        'num_classes':28,
+        'se_block': se.SELayer.None,
+        'drop_out':0,2}
+    :type params: dict
+    :param se_block_type: Squeeze and Excite block type to be included, defaults to None
+    :type se_block_type: str, valid options are {'NONE', 'CSE', 'SSE', 'CSSE'}, optional
+    :return: output tensor with maxpool, output tensor without maxpool, indices for unpooling
+    :rtype: torch.tensor [FloatTensor], torch.tensor [FloatTensor], torch.tensor [LongTensor]
+    """
+
+    def __init__(self, params, se_block_type=None):
+        super(EncoderBlock, self).__init__(params, se_block_type=se_block_type)
+        self.maxpool = nn.MaxPool2d(
+            kernel_size=params['pool'], stride=params['stride_pool'], return_indices=True)
+
+    def forward(self, input, weights=None):
+        """Forward pass
+
+        :param input: Input tensor, shape = (N x C x H x W)
+        :type input: torch.tensor [FloatTensor]
+        :param weights: Weights used for squeeze and excitation, shape depends on the type of SE block, defaults to None
+        :type weights: torch.tensor, optional
+        :return: output tensor with maxpool, output tensor without maxpool, indices for unpooling
+        :rtype: torch.tensor [FloatTensor], torch.tensor [FloatTensor], torch.tensor [LongTensor]
+        """
+
+        out_block = super(EncoderBlock, self).forward(input)
+        if self.SELayer:
+            out_block = self.SELayer(out_block, weights)
+
+        if self.drop_out_needed:
+            out_block = self.drop_out(out_block)
+
+        out_encoder, indices = self.maxpool(out_block)
+        return out_encoder, out_block, indices
+
+
+class DecoderBlock(DenseBlock):
+    """Dense decoder block with maxunpool and an optional skip connections and SE block
+
+    :param params: {
+        'num_channels':1,
+        'num_filters':64,
+        'kernel_h':5,
+        'kernel_w':5,
+        'stride_conv':1,
+        'pool':2,
+        'stride_pool':2,
+        'num_classes':28,
+        'se_block': se.SELayer.None,
+        'drop_out':0,2}
+    :type params: dict
+    :param se_block_type: Squeeze and Excite block type to be included, defaults to None
+    :type se_block_type: str, valid options are {'NONE', 'CSE', 'SSE', 'CSSE'}, optional
+    :return: forward passed tensor
+    :rtype: torch.tensor [FloatTensor]
+    """
+
+    def __init__(self, params, se_block_type=None):
+        super(DecoderBlock, self).__init__(params, se_block_type=se_block_type)
+        self.unpool = nn.MaxUnpool2d(
+            kernel_size=params['pool'], stride=params['stride_pool'])
+
+    def forward(self, input, out_block=None, indices=None, weights=None):
+        """Forward pass
+
+        :param input: Input tensor, shape = (N x C x H x W)
+        :type input: torch.tensor [FloatTensor]
+        :param out_block: Tensor for skip connection, shape = (N x C x H x W), defaults to None
+        :type out_block: torch.tensor [FloatTensor], optional
+        :param indices: Indices used for unpooling operation, defaults to None
+        :type indices: torch.tensor, optional
+        :param weights: Weights used for squeeze and excitation, shape depends on the type of SE block, defaults to None
+        :type weights: torch.tensor, optional
+        :return: Forward passed tensor
+        :rtype: torch.tensor [FloatTensor]
+        """
+
+        unpool = self.unpool(input, indices)
+        concat = torch.cat((out_block, unpool), dim=1)
+        out_block = super(DecoderBlock, self).forward(concat)
+
+        if self.SELayer:
+            out_block = self.SELayer(out_block, weights)
+
+        if self.drop_out_needed:
+            out_block = self.drop_out(out_block)
+        return out_block
+
 class SoftQuickFCN(BaseModel):
     """
     A PyTorch implementation of QuickNAT
@@ -27,23 +244,20 @@ class SoftQuickFCN(BaseModel):
         """
         super(SoftQuickFCN, self).__init__()
 
-        self.encode1 = sm.EncoderBlock(params, se_block_type=se.SELayer.CSSE)
-        params['num_channels'] = 64
-        self.encode2 = sm.EncoderBlock(params, se_block_type=se.SELayer.CSSE)
-        self.encode3 = sm.EncoderBlock(params, se_block_type=se.SELayer.CSSE)
-        self.encode4 = sm.EncoderBlock(params, se_block_type=se.SELayer.CSSE)
-        self.bottleneck = sm.DenseBlock(params, se_block_type=se.SELayer.CSSE)
-        params['num_channels'] = 128
-        ############Segmentation Task############
-        self.decode1 = sm.DecoderBlock(params, se_block_type=se.SELayer.CSSE)
-        self.decode2 = sm.DecoderBlock(params, se_block_type=se.SELayer.CSSE)
-        self.decode3 = sm.DecoderBlock(params, se_block_type=se.SELayer.CSSE)
-        self.decode4 = sm.DecoderBlock(params, se_block_type=se.SELayer.CSSE)
-        params['num_channels'] = 64
+        self.encode1 = EncoderBlock(params, se_block_type=se.SELayer.CSSE)
+        params['num_channels'] = params['num_filters']
+        self.encode2 = EncoderBlock(params, se_block_type=se.SELayer.CSSE)
+        self.encode3 = EncoderBlock(params, se_block_type=se.SELayer.CSSE)
+        self.bottleneck = DenseBlock(params, se_block_type=se.SELayer.CSSE)
+        params['num_channels'] = 2 * params['num_filters']
+        self.decode1 = DecoderBlock(params, se_block_type=se.SELayer.CSSE)
+        self.decode2 = DecoderBlock(params, se_block_type=se.SELayer.CSSE)
+        self.decode3 = DecoderBlock(params, se_block_type=se.SELayer.CSSE)
+        params['num_channels'] = params['num_filters']
         self.segmenter = sm.ClassifierBlock(params)
         ############Classification Task############
         self.classifier = nn.Sequential(
-            nn.Linear(40000, 25),
+            nn.Linear(params['num_channels'] * 50 * 50, 25),
             nn.PReLU(),
             nn.Linear(25,3)
         )
@@ -58,13 +272,11 @@ class SoftQuickFCN(BaseModel):
         e1, out1, ind1 = self.encode1.forward(input)
         e2, out2, ind2 = self.encode2.forward(e1)
         e3, out3, ind3 = self.encode3.forward(e2)
-        e4, out4, ind4 = self.encode4.forward(e3)
 
-        bn = self.bottleneck.forward(e4)
+        bn = self.bottleneck.forward(e3)
 
         ############Segmentation Task############
-        d4 = self.decode4.forward(bn, out4, ind4)
-        d3 = self.decode1.forward(d4, out3, ind3)
+        d3 = self.decode1.forward(bn, out3, ind3)
         d2 = self.decode2.forward(d3, out2, ind2)
         d1 = self.decode3.forward(d2, out1, ind1)
         prob = self.segmenter.forward(d1)
